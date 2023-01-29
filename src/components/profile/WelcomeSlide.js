260c9856ec7ccf9b8c03f896371e19dab17cb348
Juyeon Kim <kkjuyeon@gmail.com>
2023-01-30 01:15:45 +0900

    style: bouncing effect 수정
    deprecated된 바운싱 효과 수정

--- src/components/profile/WelcomeSlide.js
+++ src/components/profile/WelcomeSlide.js
@@ -1,81 +1,86 @@
-import { useEffect } from "react"
-import styled from "styled-components"
-import Typewriter from 'typewriter-effect'
+import { useEffect } from "react";
+import styled from "styled-components";
+import Typewriter from "typewriter-effect";
 
+const WelcomeSlide = ({ setSkip, i18n }) => {
+  const handleSkip = () => {
+    setSkip(true);
+  };
 
-const WelcomeSlide = ({setSkip, i18n}) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSkip(true);
+    }, 10000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
 
-    const handleSkip = () => {
-        setSkip(true);
-    }
-
-    useEffect(()=>{
-        const timer = setTimeout(()=>{
-            setSkip(true);
-        }, 10000);
-        return ()=>{
-            clearTimeout(timer);
-        }
-    },[]);
-
-    return (
-        <WelcomeSlideStyle onClick={handleSkip}>
-            <h2>This is Juyeon👩‍💻</h2>
-            <div>
-                <h3>who wants to&nbsp;</h3>
-                <Typewriter
-                    onInit={(typewriter)=>{
-                        typewriter.pauseFor(800)
-                        .typeString('be a changemaker').start()
-                        .pauseFor(100)
-                        .deleteAll()
-                        .typeString('solve a big world problem').start()
-                        .pauseFor(100)
-                        .deleteAll();
-                    }}
-                    options={{
-                        autoStart: true,
-                        loop: true,
-                    }}
-                />
-            </div>
-            <small>({i18n.t("profile.skip")})</small>
-        </WelcomeSlideStyle>
-    )
-} 
+  return (
+    <WelcomeSlideStyle onClick={handleSkip}>
+      <h2>This is Juyeon👩‍💻</h2>
+      <div>
+        <h3>who wants to&nbsp;</h3>
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .pauseFor(800)
+              .typeString("be a changemaker")
+              .start()
+              .pauseFor(100)
+              .deleteAll()
+              .typeString("solve a big world problem")
+              .start()
+              .pauseFor(100)
+              .deleteAll();
+          }}
+          options={{
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </div>
+      <small>({i18n.t("profile.skip")})</small>
+    </WelcomeSlideStyle>
+  );
+};
 
 const WelcomeSlideStyle = styled.div`
-    position: relative;
-    width: 100%;
-    height: 100%;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-style: italic;
+  cursor: pointer;
+  & > h2 {
+    margin: 0;
+    margin-bottom: 5vh;
+  }
+  & > div {
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    & > h3 {
+      margin: 0;
+    }
+  }
+  & > small {
+    opacity: 25%;
+    position: absolute;
+    bottom: 15%;
+  }
+  .Typewriter__wrapper,
+  .Typewriter__cursor {
+    color: #f08c6f;
+    font-size: calc(6px + 1.5vw) !important;
+    font-weight: 800;
     font-style: italic;
-    cursor: pointer;
-    & > h2{
-        margin: 0;
-        margin-bottom: 5vh;
-    }
-    & > div {
-        display: flex;
-        & > h3 {
-            margin: 0;
-        }
-    }
-    & > small {
-        opacity: 25%;
-        position: absolute;
-        bottom: 15%;
-    }
-    .Typewriter__wrapper, .Typewriter__cursor{
-        color: #F08C6F;
-        font-size: calc(6px + 1.5vw) !important;
-        font-weight: 800;
-        font-style: italic;
-    }
-`
-
+  }
+  & > div {
+    display: flex;
+    align-items: center;
+  }
+`;
 
 export default WelcomeSlide;
--- src/components/styles/AnimatePage.js
+++ src/components/styles/AnimatePage.js
@@ -45,67 +45,67 @@ export const UpSlide = ({ children }) => {
   );
 };
 
-
-export const Bounce = ({children}) => {
+export const Bounce = ({ children }) => {
   const transitionValues = {
     duration: 0.5,
-    yoyo: Infinity,
+    repeatType: "mirror",
+    repeat: Infinity,
     ease: "easeInOut",
   };
-  return(
-  <motion.div
-    transition={{
-      delay: 0.5,
-      y: transitionValues,
-    }}
-    animate={{
-      y: ["-1.5vh","2.5vh"],
-    }}
-    style={{
-      width:"100%",
-      textAlign:"center"
-    }}
-  >{children}
-  </motion.div>
-  )
-}
-
-export const BounceUp = ({children}) => {
-  const transitionValues = {
-    duration: 0.5,
-    yoyo: Infinity,
-    ease: "easeInOut",
-  };
-  return(
-  <motion.div
-    transition={{
-      delay: 0.5,
-      y: transitionValues,
-    }}
-    animate={{
-      y: ["0rem","-1rem"],
-    }}
-  >{children}
-  </motion.div>
-  )
-}
-
-
-
-export const Rotate360 = ({children}) => {
-    return(
-      <motion.div
+  return (
+    <motion.div
+      transition={{
+        delay: 0.5,
+        y: transitionValues,
+      }}
+      animate={{
+        y: ["-1.5vh", "2.5vh"],
+      }}
       style={{
-          display:"flex",
-          justifyContent:"center",
-          alignItems:"center",
-          fontWeight:"600",
-          color:"white",
-          height:"100%",
-          textShadow:SHADOW,
+        width: "100%",
+        textAlign: "center",
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export const BounceUp = ({ children }) => {
+  const transitionValues = {
+    duration: 0.5,
+    yoyo: Infinity,
+    ease: "easeInOut",
+  };
+  return (
+    <motion.div
+      transition={{
+        delay: 0.5,
+        y: transitionValues,
+      }}
+      animate={{
+        y: ["0rem", "-1rem"],
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export const Rotate360 = ({ children }) => {
+  return (
+    <motion.div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontWeight: "600",
+        color: "white",
+        height: "100%",
+        textShadow: SHADOW,
       }}
       initial={{ scale: 0 }}
-      animate={{ rotate: 360, scale:1 }}
+      animate={{ rotate: 360, scale: 1 }}
       transition={{
         repeat: Infinity,
         repeatDelay: 1,
@@ -113,49 +113,49 @@ export const Rotate360 = ({children}) => {
         type: "spring",
         stiffness: 100,
         scale: {
-            duration: 1,
-          },
+          duration: 1,
+        },
       }}
-      >
-        {children}
-      </motion.div>
-    )
-}
-
-export const TextTitleAnimate = ({children, duration}) => {
-  return (
-    <motion.div 
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true }}
-        variants={{
-            visible: { opacity: 1, scale: 1 },
-            hidden: { opacity: 0, scale: 0 }
-        }}
-        transition={{
-            default: {
-                duration: duration,
-                ease: [0, 0.71, 0.2, 1.01]
-            },
-            scale: {
-                type: "spring",
-                damping: 5,
-                stiffness: 100,
-                restDelta: 0.001
-            }
-        }}
     >
-    {children}
-  </motion.div>
-  )
-}
+      {children}
+    </motion.div>
+  );
+};
 
-export const FadeIn = ({children}) => {
+export const TextTitleAnimate = ({ children, duration }) => {
   return (
     <motion.div
-      initial={{ opacity: 0, y:25 }}
-      exit={{ opacity: 1, y:0 }}
-      animate={{opacity: 1, y:0}}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      variants={{
+        visible: { opacity: 1, scale: 1 },
+        hidden: { opacity: 0, scale: 0 },
+      }}
+      transition={{
+        default: {
+          duration: duration,
+          ease: [0, 0.71, 0.2, 1.01],
+        },
+        scale: {
+          type: "spring",
+          damping: 5,
+          stiffness: 100,
+          restDelta: 0.001,
+        },
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export const FadeIn = ({ children }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 25 }}
+      exit={{ opacity: 1, y: 0 }}
+      animate={{ opacity: 1, y: 0 }}
       transition={{
         y: {
           ease: "linear",
@@ -165,46 +165,56 @@ export const FadeIn = ({children}) => {
     >
       {children}
     </motion.div>
-  )
-}
+  );
+};
 
-export const ButtonHoverScale = ({children}) => {
+export const ButtonHoverScale = ({ children }) => {
   return (
-    <motion.div 
-    initial={{ opacity: 0.7, scale: 1 }}
-    whileHover={{ opacity: 1, scale: 1.2, x:-50 }}
-    style={{position:"sticky", bottom:"4vh", display:"flex",justifyContent:"end", margin:"0 2vw"}}>
-    <em style={{ 
-        width: "fit-content", 
-        backgroundColor: "#FFE2D9", 
-        padding:"1vh 1vw",
-        borderRadius:"0.8vw",
-        cursor:"pointer",
-        display:"flex",
-        justifyContent:"center",
-    }}>
+    <motion.div
+      initial={{ opacity: 0.7, scale: 1 }}
+      whileHover={{ opacity: 1, scale: 1.2, x: -50 }}
+      style={{
+        position: "sticky",
+        bottom: "4vh",
+        display: "flex",
+        justifyContent: "end",
+        margin: "0 2vw",
+      }}
+    >
+      <em
+        style={{
+          width: "fit-content",
+          backgroundColor: "#FFE2D9",
+          padding: "1vh 1vw",
+          borderRadius: "0.8vw",
+          cursor: "pointer",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
         {children}
-    </em>
-</motion.div>
-  )
-}
+      </em>
+    </motion.div>
+  );
+};
 
-
-export const ShakingImageAnimate = ({clicked, src}) => {
+export const ShakingImageAnimate = ({ clicked, src }) => {
   return (
-    <motion.img 
-        animate = {{
-        rotateY: clicked? [90,null] : [90,180],
-        rotateX: clicked? [10,-10,10,-10,5,-5,3,-3] : [-10,10,-10,10,-5,5,-3,3],
+    <motion.img
+      animate={{
+        rotateY: clicked ? [90, 0] : [90, 180],
+        rotateX: clicked
+          ? [10, -10, 10, -10, 5, -5, 3, -3]
+          : [-10, 10, -10, 10, -5, 5, -3, 3],
         opacity: 1,
         duration: 0.5,
-        transition:{
-            ease: "circOut",
-        }
-        }}
-        alt="byte img"
-        src={src}
-        style={{height:"23vh",minWidth:"50px", opacity:"80%"}}
+        transition: {
+          ease: "circOut",
+        },
+      }}
+      alt="byte img"
+      src={src}
+      style={{ height: "23vh", minWidth: "50px", opacity: "80%" }}
     />
-  )
-}
+  );
+};
